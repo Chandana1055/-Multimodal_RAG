67ea714f32a0bb96e661da965143f1c7d5dc2f22
Chandana SN <chandanasn795@gmail.com>
2026-02-13 14:42:00 +0530

    Create embddings.py

---
+++ rag/embddings.py
@@ -0,0 +1,9 @@
+from sentence_transformers import SentenceTransformer
+
+model = SentenceTransformer("all-MiniLM-L6-v2")
+
+def get_text_embedding(text):
+    return model.encode(text)
+
+def get_embeddings(text_list):
+    return model.encode(text_list)
